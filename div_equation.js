6a3f784daee385adc361e3dc48a18f6182a54dfd
YEJIN <leeyjin0926@gmail.com>
2024-04-18 14:36:38 +0900

    lim 및 예제 수식 추가

--- div_equation.js
+++ div_equation.js
@@ -14,9 +14,11 @@ const functions = {
     getfracEndIndex: getFracEndIndex,
     getsqrtEndIndex: getSqrtEndIndex,
     getleftEndIndex: getLeftEndIndex,
+    getlimEndIndex: getLimEndIndex,
 
     readfrac: readFrac,
     readsqrt: readSqrt,
+    readlim: readLim,
 }
  
 // let equation = "x=\\frac{-b \\pm \\sqrt{b^2 -4ac}}{2a}"
@@ -34,7 +36,10 @@ const functions = {
 // var equation = 'x^2+2x + 1'
 // var equation = "\\frac{n!}{k!(n-k)!} = \\binom{n}{k} = _{n}\\mathrm{C}_{k}"
 // var equation = "f^{\\prime}(x)=\lim_{h \\to 0}\\frac{f(x+h)-(x)}{h}"
-var equation = " x = \\frac{\\frac{1\\times 2}{1+x}}{4ac + \\sqrt{x+2}}\\pm-b"
+// var equation = " x = \\frac{\\frac{1\\times 2y}{1+x}}{4ac + \\sqrt{x+2}}\\pm b"
+// var equation = "\\frac{ 1 }{ \\sqrt { 2 }+\\frac{ 1 }{ \\sqrt { 2 } +\\frac { 1 }{ \\sqrt { 2 } + 1}}} "
+// var equation = "2\\times2 + 4xy - \\sqrt{4 + \\sqrt{x+2}} + \\frac{-b \\pm \\sqrt{b -4ac}}{2a}"
+// var equation = "3a +  \\lim_{x\\to0} \\frac{2x}{3a}"
 //////////////////////////////////////////////
 //////////////////////////////////////////////
 //#endregion
@@ -149,6 +154,35 @@ function getSqrtEndIndex(expression, idx) {
     return endIdx;
 }
 
+function getLimEndIndex(expression, idx){
+    let stack = [];
+    var braceCnt = 0
+    var endIdx = 0;
+
+    while (idx < expression.length) {
+        if (expression[idx] === "{") {
+            if (stack.length === 0 && braceCnt < 1) {
+                // braceCnt += 1;
+            }
+            stack.push("{");
+        }
+        else if (expression[idx] === "}") {
+            stack.pop();
+            // 분모까지 마무리
+            if (stack.length === 0) {
+                braceCnt += 1;
+                if (braceCnt == 1) {
+                    endIdx = idx
+                    break
+                }
+            }
+        }
+        idx += 1;
+    }
+    return endIdx;
+
+}
+
 // 특수경우: 우측에 쌍이 있음. 시작은 \\left
 // math_expression_pair 에서 어떤것인지 확인하고
 function getLeftEndIndex(expression, idx) {
@@ -235,7 +269,7 @@ function matchText(char){
 } 
 //#endregion
 
-// #region 명령어 텍스트 변환 함수 
+//#region 명령어 텍스트 변환 함수 
 /** 분수 **/
 function readFrac(formula){
     var command = [];
@@ -295,6 +329,32 @@ function readSqrt(formula){
 
     return text;
 }
+
+/** 리미트 **/
+function readLim(formula){
+    var command = [];
+    var idx = formula.indexOf("\\to");
+    var start = formula.slice(6, idx);
+    var end = formula.slice(idx + 4, -1); 
+
+    let text = "리미트 ";
+
+    let splitExp = splitExpression(start, command);
+    splitExp.forEach(function(element){
+        text += convertElement(element, command);
+    })
+
+    text += "가 ";
+
+    splitExp = splitExpression(end, command);
+    splitExp.forEach(function(element){
+        text += convertElement(element, command);
+    })
+
+    text += "으로 갈 때 ";
+
+    return text;
+}
 // #endregion
  
 //#region MAIN_FUNC
@@ -321,7 +381,15 @@ function convert2Text(expression){
     })
 
     console.log("수식: ", expression);
+    console.log("분해: ", initExp);
     console.log(res);
+
+    // 합
+    let convertedTEXT = "";
+    res.forEach(function(element){
+        convertedTEXT += element;
+    })
+    console.log("결과: ", convertedTEXT);
 }
 
 // 괄호를 고려해서 하나씩 진행 하다가
